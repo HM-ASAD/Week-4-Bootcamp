2276648a64dd1e0095a0998328e6ae5baf08f473
HM Asad <hmasad.90@gmail.com>
2022-10-09 23:18:24 +0500

    Assignment 3

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Think from './Think';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className='think'>
+   <Think />
+   </div>
+    </div>
+  );
+}
+
+export default App;
